f38cb2a7df8d490406faf1f487f6631c2f70062c
Brice Dobrowolski <brice.dobrowolski@yahoo.fr>
2021-12-02 09:12:55 +0100

    update fonctionne

--- src/main/java/manager/StaffManager.java
+++ src/main/java/manager/StaffManager.java
@@ -100,7 +100,7 @@ public class StaffManager {
 		
 		// requete HQL qui récupère tous les employés
 		Query query = session.createQuery("select s from Staff s");
-		// liste des staff récupérés
+		// liste des staffs récupérés
 		List<Staff> staffs = query.getResultList();
 		
 		session.close();
@@ -124,6 +124,18 @@ public class StaffManager {
 		}
 		if(newStaff.getEmail() != null && staff.getEmail() != newStaff.getEmail()) {
 			staff.setEmail(newStaff.getEmail());
+		}
+		if(newStaff.getAge() != 0 && staff.getAge() != newStaff.getAge()) {
+			staff.setAge(newStaff.getAge());
+		}
+		if(newStaff.getFonction() != null && staff.getFonction() != newStaff.getFonction()) {
+			staff.setFonction(newStaff.getFonction());
+		}
+		if(newStaff.getTelephone() != null && staff.getTelephone() != newStaff.getTelephone()) {
+			staff.setTelephone(newStaff.getTelephone());
+		}
+		if(newStaff.getAdresse() != null && staff.getAdresse() != newStaff.getAdresse()) {
+			staff.setAdresse(newStaff.getAdresse());
 		}
 		
 		Session session = sessionFactory.openSession();
@@ -156,17 +168,29 @@ public class StaffManager {
 		//manager.create(); // create fonctionne
 		//manager.read(1); // read fonctionne
 		
-		/**
-		 * TODO la méthode update
-		 */
-		Staff staff = new Staff();
+						// Update fonctionne
 		
-//		List<Staff> staffs = manager.readAll(); // readAll fonctionne
+//		Staff staff = new Staff();		
+//		staff.setNom("NomChangé!!");
+//		staff.setPrenom("Valjean");
+//		staff.setEmail("NomChangé!!@jean.fr");
+//		staff.setAge(45);
+//		staff.setFonction("CEO");
+//		staff.setTelephone("0607080910");
+//		staff.setAdresse("10 rue qqpart");
+//		int id = 2;
+//		manager.update(id, staff);	
+		
+						// readAll fonctionne
+		
+//		List<Staff> staffs = manager.readAll(); 
 //		for (Staff staff : staffs) {
 //			System.out.println("L'employé n° "+staff.getId()+" s'appelle "+staff.getNom()+" "+staff.getPrenom() );
 //		}
 		
-		//Staff staff = manager.read(1); delete fonctionne
+						//delete fonctionne
+		
+		//Staff staff = manager.read(1); 
 		//manager.delete(staff);
 	}
 }
